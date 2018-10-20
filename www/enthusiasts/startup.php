2c8728f93824da5f053131c90b861ca94afd0b37
mudit <mud.00it@gmail.com>
2018-10-20 15:41:05 +0530

    Registration styling

--- www/enthusiasts/startup.php
+++ www/enthusiasts/startup.php
@@ -1,15 +1,28 @@
 <html>
 <head><title> Registeration</title>
+  <style>
+
+  p{
+    text-align: center;
+    margin-top: 200px;
+  	max-width: 500px;
+  	padding: 10px 20px;
+  	background: #f4f7f8;
+  	margin: 100px auto;
+  	padding: 20px;
+  	background: #f4f7f8;
+  	border-radius: 8px;
+  	font-family: Georgia, "Times New Roman", Times, serif;
+  }
+  body{
+  	background-color: #AC58FA;
+  }
+  </style>
 </head>
 <body>
-<p> You are been successfully registered.<br>
-Thankyou,and welcome on board.<br><br><br>
-
-We'll keep you posted.<br>
-</p>
-
-<form method="post" action="file:///home/mudit/Desktop/ecellgdgtbi/www/index.html">
-  <input type="submit" value="GO HOME">
-</form>
+<p><b> You are been successfully registered.<br><br>
+Thankyou,<br>   and welcome on board.<br><br><br>
+We'll keep you posted.<br></p>
+</b>
 </body>
 </html>
