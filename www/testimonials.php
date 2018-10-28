b6d8bea2c2262b415ec58117cccae0cfbde501b7
SwG Ghosh <codecrafts.cf@icloud.com>
2018-10-29 00:04:40 +0530

    Fully refactored testimonials to use with header, footer and proper css

---
+++ www/testimonials.php
@@ -0,0 +1,115 @@
+<?php
+$title = 'Testimonials';
+$stylesheets = array('/stylesheets/testimonials.css');
+include('header.php');
+?>
+
+    <div class="bg">
+    <h1 align="center"> Testimonials</h1>
+
+    <table>
+    <tr>
+    <td>
+    <div class="container">
+    <p><span> Phonewalaa.com </span></p>
+    <p>An e-commerce site, one stop shop for all the smart phone related needs. Currently the weekly sales are 100-300 mobile covers. The company has finalized offline and online distributors to enhance sales. An app and a Universal payment system connected to bank account is also under process. A collaboration with Case Master (Sales Source) has been done. Phonewalaa.com is also collaborating with Instagram handles (@fashioninstadiary, @hercreativepalace, @thegreyhalfway, @missfoodiediaries, @einzinaffair) which have high amounts of followers to boost brand awareness and marketing.</p>
+    </div>
+    </td>
+    </tr>
+    <tr>
+
+    <td>
+    <div class="container">
+    <p><span>Mahalaxmi Enterprises</span></p>
+    <p> A manufacturer and supplier of fly ash bricks and red bricks, got registered in 2014. The office is located at dundahera village, old Gurgaon Delhi road and the plant is situated in Rajasthan. Several projects like Ireo project sector 67, DLF project sector 81, Mtech project, cosmos project etc are some of the projects in Haryana and Rajasthan being undertaken by Mahalaxmi enterprises.<br></p>
+    </div>
+    </td>
+    <tr>
+    <td>
+    <div class="container">
+    <p><span> Coverdekho.com </span></p>
+    <p>Another entrepreneurship venture for customised mobile phone covers, operational in New Delhi.</p>
+    </div>
+    </td>
+    </tr>
+    <tr>
+
+    <td>
+    <div class="container">
+    <p><span>DessertsDaily</span></p>
+    <p>A company which home delivers desserts and chocolates.</p>
+    </div>
+    </td>
+    </tr>
+    <tr>
+    <td> <div class="container">
+    <p><span>Krishna Wedding Showroom</span></p>
+    <p>An exclusive wedding and party wear located in Khndsa road, Gurgaon. The products from this showroom are also available on shoplclues.com and snapdeal.com.</p>
+    </div> </td>
+    </tr>
+    <tr>
+    <td> <div class="container">
+    <p><span>Snackrabbit.in</span></p>
+    <p>An online distributor of exotic snacks in the NCR region. It has tie ups with over 100 leading national and international brands some of which include Haldiram™, Tong Garden, Sofit, Nescafe, Nestle, Mars etc. It delivers all over India.</p>
+    </div> </td>
+
+    </tr>
+    <tr>
+    <td>
+    <div class="container">
+    <p><span>Scent Studio</span></p>
+    <p>Bespoke room freshener and perfumes, operational in Cannaught place, New Delhi. Scent Studio has already applied for a registered trade mark, and managed to have an official sale of 2.5 lacs in the first month of opening. Expecting it to be 35 lacs in the first year. </p>
+    </div>
+    </td>
+    </tr>
+    <tr>
+    <td>
+    <div class="container">
+    <p><span>Moonrock Events</span></p>
+    <p>An EventManagement Company, got registered in Year 2012 by the name of Moonrock Events. Some of the services provided by Moonrock events are Artist Management Services, Event Planning Services, Event Promotion, event Marketing, Club Openings, Theme parties, road shows etc</p>
+    </div>
+    </td>
+
+
+    </tr>
+    <tr>
+    <td>
+    <div class="container">
+    <p><span>Online Property Mart (OPM)</span></p>
+    <p>Onlinepropertymart.com, Real estate deals in office, home and farmland. </p>
+    </div>
+    </td>
+    </tr>
+    <tr>
+    <td>
+    <div class="container">
+    <p><span>EJS Clothing </span></p>
+    <p>Operational in Moanda, Democratic Republic of the Congo, EJS clothing is a complete store for Party and wedding dresses on rent. The store is expected to be registered by 2020.</p>
+    </div>
+    </td>
+
+    </tr>
+    <tr>
+    <td>
+    <div class="container">
+    <p><span>Endymion Exports Private Ltd</span></p>
+    <p>An export house for leather apparels.</p>
+    </div>
+    </td>
+    </tr>
+    <tr>
+    <td>
+    <div class="container">
+    <p><span>Hire Bazaar</span></p>
+    <p>Provides home, office and party rental solutions.</p>
+    </div>
+    </td>
+
+    </tr>
+    </table>
+    </div>
+
+<?php
+$scripts = array();
+include('footer.php');
+?>
