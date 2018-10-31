efda8494018fee5c90a9c7acad0e0db6447a2601
Aryan Agarwal <aryan27agarwal@gmail.com>
2018-11-01 00:37:27 +0530

    Added SupportTeam page to www and minor changes to contact.php and enthusiast

--- www/enthusiasts.php
+++ www/enthusiasts.php
@@ -3,17 +3,6 @@ $title = 'Testimonials';
 $stylesheets = array('/stylesheets/enthusiasts.css');
 include('header.php');
 ?>
-<html>
-<head>
- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Enthusiasts </title>
-
-
-</head>
-<body>
 <div class="background fm">
 <div class="form">
  <form method="post" action="startup.php"  autocomplete="on">
@@ -47,8 +36,6 @@ document.getElementById("Submit").disabled=false;
 </script>
 </div>
 </div>
-</body>
-</html>
 <?php
 $scripts = array();
 include('footer.php');
---
+++ www/supportteam.php
@@ -0,0 +1,123 @@
+<?php
+$title = 'Contact';
+$stylesheets = array('/stylesheets/team.css');
+include('header.php');
+?>
+  <div class="background team">
+      <br>
+      <div class="row">
+        <div class="col-sm-6">
+          <div class="container">
+            <h2>Neelesh Agarwal</h2>
+            <p class="title">President</p>
+          </div>
+        </div>
+        <div class="col-sm-6">
+          <div class="container">
+            <h2>Anubhav Gupta</h2>
+            <p class="title">Vice President</p>
+          </div>
+        </div>
+
+      </div>
+      <br>
+      <h1>Public Relations</h1>
+      <div class="row">
+        <div class="col-sm-4">
+
+          <div class="container">
+            <h2>Aman Mathur</h2>
+          </div>
+        </div>
+
+        <div class="col-sm-4">
+          <div class="container">
+            <h2>Ojasvi Gautam</h2>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="container">
+            <h2>Apurva Maroria</h2>
+          </div>
+        </div>
+      </div>
+      <br>
+      <div class="row">
+        <div class="col-sm-6">
+          <div class="container">
+            <h2>Anees Nakai</h2>
+          </div>
+        </div>
+        <div class="col-sm-6">
+          <div class="container">
+            <h2>Priyansh Sethi</h2>
+          </div>
+        </div>
+
+      </div>
+      <br>
+      <h1>Marketing</h1>
+      <div class="row">
+        <div class="col-sm-6">
+          <div class="container">
+            <h2>Bhavya Tandon</h2>
+          </div>
+        </div>
+        <div class="col-sm-6">
+          <div class="container">
+            <h2>Komal Kumari</h2>
+          </div>
+        </div>
+      </div>
+      <br>
+      <h1>Media</h1>
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="container">
+            <h2>Ayushman Gupta</h2>
+          </div>
+        </div>
+      </div>
+      <br>
+      <h1>Web Development</h1>
+      <div class="row">
+        <div class="col-sm-6">
+          <div class="container">
+            <h2>Swarup Ghosh</h2>
+          </div>
+        </div>
+        <div class="col-sm-6">
+          <div class="container">
+            <h2>Smahi Shah</h2>
+          </div>
+        </div>
+      </div>
+      <br>
+      <div class="row">
+        <div class="col-sm-6">
+            <div class="card">
+              <img src="images/mudit.png" alt="John" style="width:20%">
+            <h2>Mudit Khandelwal</h2>
+          </div>
+      </div>
+        <div class="col-sm-6">
+            <div class="card">
+          <img src="images/aryan.png" alt="John" style="width:20%">
+            <h2>Aryan Agarwal</h2>
+          </div>
+      </div>
+    </div>
+      <br>
+      <h1>Sponsers</h1>
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="container">
+            <h2>Nishant Vashisht</h2>
+          </div>
+        </div>
+      </div>
+  </div>
+<?php
+$scripts = array();
+include('footer.php');
+?>
