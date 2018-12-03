58efc3799b96bf3a592d82711bcd6e26b380daba
SwG Ghosh <codecrafts.cf@icloud.com>
2018-12-03 11:23:06 +0530

    Test fb login

--- www/portal/index.php
+++ www/portal/index.php
@@ -10,6 +10,10 @@
         <div class="g-signin2" data-onsuccess="onSignIn"></div>
         <br>
         <a href="#" onclick="signOut();">Sign out</a>
+        <!---->
+        <div id="fb-root"></div>
+        <div class="fb-login-button" data-max-rows="1" data-size="large" data-button-type="login_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false"></div>
+        <button id="test" onclick="clickButton();"></button>
     </body>
     <script src="https://apis.google.com/js/platform.js" async defer></script>
     <script>
@@ -28,5 +32,25 @@
             console.log('User signed out.');
             });
         }
+    </script>
+    <script>
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v3.2&appId=254027531936489&autoLogAppEvents=1';
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
+    <script>
+        function clickButton() {
+            FB.getLoginStatus(function(response) {
+                console.log(response);
+            });
+        }
+        function later() {
+            console.log('done');
+        }
+        FB.Event.subscribe('xfbml.render', later);
     </script>
 </html>
