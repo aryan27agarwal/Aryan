94fa02c24d41fe3ad026e1e455d2c378496d53fc
Swarup Ghosh <codecrafts.cf@icloud.com>
2019-01-08 14:58:56 +0530

    Modified supportteam.php

--- www/supportteam.php
+++ www/supportteam.php
@@ -3,100 +3,89 @@ $title = 'Contact';
 $stylesheets = array('/stylesheets/team.css');
 include('header.php');
 ?>
-  <div class="background team">
-      <br>
-      <div class="row">
-        <div class="col-sm-6">
-          <div class="container">
-            <h2>Neelesh Agarwal</h2>
-            <p class="title">President</p>
-          </div>
-        </div>
-        <div class="col-sm-6">
-          <div class="container">
-            <h2>Anubhav Gupta</h2>
-            <p class="title">Vice President</p>
-          </div>
-        </div>
 
-      </div>
-      <br>
-      <h1>Public Relations</h1>
-      <div class="row">
-        <div class="col-sm-4">
-
-          <div class="container">
-            <h2>Aman Mathur</h2>
-          </div>
-        </div>
-
-        <div class="col-sm-4">
-          <div class="container">
-            <h2>Ojasvi Gautam</h2>
-          </div>
-        </div>
-        <div class="col-sm-4">
-          <div class="container">
-            <h2>Apurva Maroria</h2>
-          </div>
-        </div>
-      </div>
-      <br>
-      <div class="row">
-        <div class="col-sm-6">
-          <div class="container">
-            <h2>Anees Nakai</h2>
-          </div>
-        </div>
-        <div class="col-sm-6">
-          <div class="container">
-            <h2>Priyansh Sethi</h2>
-          </div>
-        </div>
-
-      </div>
-      <br>
-      <h1>Media</h1>
-      <div class="row">
-        <div class="col-sm-12">
-          <div class="container">
-            <h2>Ayushman Gupta</h2>
-          </div>
-        </div>
-      </div>
-      <br>
-      <h1>Web Development</h1>
-      <div class="row">
-        <div class="col-sm-6">
-          <div class="container">
-            <h2>Smahi Shah</h2>
-          </div>
-        </div>
-      </div>
-      <br>
-      <div class="row">
-        <div class="col-sm-6">
-            <div class="container">
-            <h2>Mudit Khandelwal</h2>
-          </div>
-      </div>
-        <div class="col-sm-6">
-            <div class="container">
-            <h2>Aryan Agarwal</h2>
-          </div>
+<div class="background team">
+  <br>
+  <div class="row">
+    <div class="col-sm-6">
+      <div class="container">
+        <h2>Neelesh Agarwal</h2>
+        <p class="title">President</p>
       </div>
     </div>
-      <br>
-      <h1>Sponsors</h1>
-      <div class="row">
-        <div class="col-sm-12">
-          <div class="container">
-            <h2>Nishant Vashisht</h2>
-          </div>
-        </div>
+    <div class="col-sm-6">
+      <div class="container">
+        <h2>Anubhav Gupta</h2>
+        <p class="title">Vice President</p>
       </div>
-      <br>
+    </div>
+
   </div>
+  <br>
+  <h1>Public Relations</h1>
+  <div class="row">
+    <div class="col-sm-4">
+
+      <div class="container">
+        <h2>Aman Mathur</h2>
+      </div>
+    </div>
+
+    <div class="col-sm-4">
+      <div class="container">
+        <h2>Ojasvi Gautam</h2>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="container">
+        <h2>Apurva Maroria</h2>
+      </div>
+    </div>
+  </div>
+  <br>
+  <div class="row">
+    <div class="col-sm-6">
+      <div class="container">
+        <h2>Anees Nakai</h2>
+      </div>
+    </div>
+    <div class="col-sm-6">
+      <div class="container">
+        <h2>Priyansh Sethi</h2>
+      </div>
+    </div>
+
+  </div>
+  <br>
+  <h1>Media</h1>
+  <div class="row">
+    <div class="col-sm-12">
+      <div class="container">
+        <h2>Ayushman Gupta</h2>
+      </div>
+    </div>
+  </div>
+  <br>
+  <h1>Web Development <small><code> <a href="https://github.com/gdgu/ecellgdgu/contributors">:. info .:</a> </code></small></h1>
+  <div class="row">
+    <div class="col-sm-12">
+      <div class="container">
+        <h2>Aryan Agarwal </h2>
+      </div>
+    </div>
+  </div>
+  <br>
+  <h1>Sponsors</h1>
+  <div class="row">
+    <div class="col-sm-12">
+      <div class="container">
+        <h2>Nishant Vashisht</h2>
+      </div>
+    </div>
+  </div>
+  <br>
+</div>
+
 <?php
 $scripts = array();
 include('footer.php');
