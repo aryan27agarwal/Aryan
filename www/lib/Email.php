e9a51ad56e8569bd72f62a43e27987a9ca47e3e1
Swarup Ghosh <codecrafts.cf@icloud.com>
2019-01-08 14:41:58 +0530

    Hide sensitive information like cloud function url using env var

--- www/lib/Email.php
+++ www/lib/Email.php
@@ -1,11 +1,10 @@
 <?php
-define('CLOUD_FUNCTION_URL', 'http://asia-northeast1-code-crafts-1477836554331.cloudfunctions.net/ecellsendemail');
+define('EMAIL_CLOUD_FUNCTION_URL', getenv('EMAIL_CLOUD_FUNCTION_URL'));
 
 class Email {
     public static function sendEmail($to, $subject, $body) {
         
         $content = compact('to', 'subject', 'body');
-        $url = CLOUD_FUNCTION_URL;
     
         $options = array(
             'http' => array(
@@ -17,7 +16,7 @@ class Email {
     
         $context = stream_context_create($options);
     
-        $result = file_get_contents($url, false, $context);
+        $result = file_get_contents(EMAIL_CLOUD_FUNCTION_URL, false, $context);
         $result = json_decode($result, true);
     
         return $result['success'];
