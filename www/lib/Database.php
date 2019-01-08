12ed14059568bee31eb54cc5d25804a451230d92
Swarup Ghosh <codecrafts.cf@icloud.com>
2019-01-08 14:12:58 +0530

    Modified lib/Database for postgres

--- www/lib/Database.php
+++ www/lib/Database.php
@@ -1,21 +1,21 @@
 <?php
 
-define('MYSQL_DATABASE_URL', getenv('MYSQL_DATABASE_URL'));
-$db_url = parse_url(MYSQL_DATABASE_URL);
+define('DATABASE_URL', getenv('DATABASE_URL'));
+$db_url = parse_url(DATABASE_URL);
 
-define('MYSQL_HOST', $db_url['host']);
-define('MYSQL_USER', $db_url['user']);
-define('MYSQL_PASSWD', $db_url['pass']);
-define('MYSQL_PORT', $db_url['port']);
-define('MYSQL_DB', substr($db_url['path'], 1));
+define('DB_HOST', $db_url['host']);
+define('DB_USER', $db_url['user']);
+define('DB_PASSWD', $db_url['pass']);
+define('DB_PORT', $db_url['port']);
+define('DB_NAME', substr($db_url['path'], 1));
 
-define('MYSQL_DSN', sprintf('mysql:host=%s;dbname=%s;port=%s;charset=utf8', MYSQL_HOST, MYSQL_DB, MYSQL_PORT));
+define('POSTGRES_DSN', sprintf('postgres:host=%s;dbname=%s;port=%s;charset=utf8', DB_HOST, DB_NAME, DB_PORT));
 
 class Database {
     private $pdo;
 
     public function __construct() {
-        $pdo = new PDO(MYSQL_DSN, MYSQL_USER, MYSQL_PASSWD, [
+        $pdo = new PDO(POSTGRES_DSN, DB_USER, DB_PASSWD, [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_PERSISTENT => true
