62743bd6fbe7261ff2b893aa6ad6348a661abe44
Swarup Ghosh <codecrafts.cf@icloud.com>
2019-01-11 01:31:02 +0530

    Added lib/TeamUpdate - ability to send message to TG group

---
+++ lib/TeamUpdate.php
@@ -0,0 +1,30 @@
+<?php
+define('BOT_TOKEN', getenv('BOT_TOKEN'));
+define('CHAT_ID', getenv('CHAT_ID'));
+
+define('SEND_MESSAGE_URL', sprintf('https://api.telegram.org/bot%s/sendMessage', BOT_TOKEN));
+
+class TeamUpdate {
+    public static function sendUpdate($text) {
+        // text supports basic markdown formatting
+        $content = [
+            'text' => $text,
+            'chat_id' => CHAT_ID,
+            'parse_mode' => 'Markdown'
+        ];
+        $options = array(
+            'http' => array(
+                'method' => 'POST',
+                'header' => 'Content-Type: application/json',
+                'content' => json_encode($content)
+            )
+        );
+
+        $context = stream_context_create($options);
+    
+        $result = file_get_contents(SEND_MESSAGE_URL, false, $context);
+        $result = json_decode($result, true);
+    
+        return $result['ok'];
+    }
+}
