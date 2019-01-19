b5c1293d28b2cf71d3af35f7af7f378204676bcb
FreeCoder111 <amitdagar311@gmail.com>
2019-01-19 08:20:55 +0530

    Config insert changes

--- www/config.php
+++ www/config.php
@@ -11,21 +11,25 @@
       echo "Opened database successfully\n";
    }
    
+//    $sql =<<<EOF
+//       CREATE TABLE users
+//       (ID SERIAL PRIMARY KEY     NOT NULL,
+//       NAME           TEXT    NOT NULL,
+//       EMAIL          VARCHAR(100) NOT NULL,
+//       AGE            INT     NOT NULL,
+//       ADDRESS        CHAR(50),
+//       SALARY         REAL);
+// EOF;
    $sql =<<<EOF
-      CREATE TABLE users
-      (ID SERIAL PRIMARY KEY     NOT NULL,
-      NAME           TEXT    NOT NULL,
-      EMAIL          VARCHAR(100) NOT NULL,
-      AGE            INT     NOT NULL,
-      ADDRESS        CHAR(50),
-      SALARY         REAL);
+      INSERT INTO users (NAME,EMAIL,AGE,ADDRESS,SALARY)
+      VALUES ('Amit','amitdagar4096@gmail.com', 32, 'Palwal', 20000.00 );
 EOF;
 
    $ret = pg_query($db, $sql);
    if(!$ret) {
       echo pg_last_error($db);
    } else {
-      echo "Table created successfully\n";
+      echo "Records created successfully\n";
    }
    pg_close($db);
 ?>
