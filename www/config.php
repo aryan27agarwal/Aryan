9c6850c2344ac8f62f09c17c366b020141f365fa
FreeCoder111 <amitdagar311@gmail.com>
2019-01-19 10:46:59 +0530

    config test-1

--- www/config.php
+++ www/config.php
@@ -1,35 +1,36 @@
 <?php
    $host        = getenv('HOST');
-   $port        = getenv('PORT');
-   $dbname      = getenv('DB_NAME');
-   $credentials = getenv('CREDENTIALS');
+   echo getenv('HOST');
+//    $port        = getenv('PORT');
+//    $dbname      = getenv('DB_NAME');
+//    $credentials = getenv('CREDENTIALS');
 
-   $db = pg_connect( "$host $port $dbname $credentials"  );
-   if(!$db) {
-      echo "Error : Unable to open database\n";
-   } else {
-      echo "Opened database successfully\n";
-   }
+//    $db = pg_connect( "$host $port $dbname $credentials"  );
+//    if(!$db) {
+//       echo "Error : Unable to open database\n";
+//    } else {
+//       echo "Opened database successfully\n";
+//    }
    
+// //    $sql =<<<EOF
+// //       CREATE TABLE users
+// //       (ID SERIAL PRIMARY KEY     NOT NULL,
+// //       NAME           TEXT    NOT NULL,
+// //       EMAIL          VARCHAR(100) NOT NULL,
+// //       AGE            INT     NOT NULL,
+// //       ADDRESS        CHAR(50),
+// //       SALARY         REAL);
+// // EOF;
 //    $sql =<<<EOF
-//       CREATE TABLE users
-//       (ID SERIAL PRIMARY KEY     NOT NULL,
-//       NAME           TEXT    NOT NULL,
-//       EMAIL          VARCHAR(100) NOT NULL,
-//       AGE            INT     NOT NULL,
-//       ADDRESS        CHAR(50),
-//       SALARY         REAL);
+//       INSERT INTO users (NAME,EMAIL,AGE,ADDRESS,SALARY)
+//       VALUES ('Amit','amitdagar4096@gmail.com', 32, 'Palwal', 20000.00 );
 // EOF;
-   $sql =<<<EOF
-      INSERT INTO users (NAME,EMAIL,AGE,ADDRESS,SALARY)
-      VALUES ('Amit','amitdagar4096@gmail.com', 32, 'Palwal', 20000.00 );
-EOF;
 
-   $ret = pg_query($db, $sql);
-   if(!$ret) {
-      echo pg_last_error($db);
-   } else {
-      echo "Records created successfully\n";
-   }
-   pg_close($db);
+//    $ret = pg_query($db, $sql);
+//    if(!$ret) {
+//       echo pg_last_error($db);
+//    } else {
+//       echo "Records created successfully\n";
+//    }
+//    pg_close($db);
 ?>
