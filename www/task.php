cff0ed0f05ce80e2c5a83a4e0bb1e0c3a4d80066
Aryan Agarwal <aryan27agarwal@gmail.com>
2019-01-21 12:20:57 +0530

    Task Page added

---
+++ www/task.php
@@ -0,0 +1,40 @@
+﻿<?php
+$title = 'Testimonials';
+$stylesheets = array('/stylesheets/testimonials.css');
+include('header.php');
+?>
+
+    <div class="bg">
+    <h1 align="center"> Hello Campus Ambassadors, here are the task you need to perform initally.</h1>
+
+    <table>
+    <tr>
+    <td>
+    <div class="container">
+    <p><span>LinkedIn Profile</span></p>
+    <p>You are required to create your LinkedIn profile</p>
+    <p>Follow E-Cell GDGU on LinkedIn </p>
+    <p>Add to your profile working as a Campus Ambassador at E-Cell GDGU by JAN -19<p>
+    </div>
+    </td>
+    </tr>
+    <tr>
+
+    <td>
+    <div class="container">
+    <p><span>Social Network</span></p>
+    <p>Follow E-Cell GDGU on Instagram @ecellgdgu</p>
+    <p>Like GDGTBI Facebook page @gdgtbi</p>
+    <p>Follow E-Cell GDGU on Twitter @ecellgdgu</p>
+    </div>
+    </td>
+    <tr>
+
+    </tr>
+    </table>
+    </div>
+
+<?php
+$scripts = array();
+include('footer.php');
+?>
