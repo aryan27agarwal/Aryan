5ede42837e568fbdddc3da5d58c45c56371ba674
Swarup Ghosh <codecrafts.cf@icloud.com>
2019-01-21 15:51:53 +0530

    Tasks updated with proper details

--- www/task.php
+++ www/task.php
@@ -1,20 +1,23 @@
 ﻿<?php
 $title = 'Campus Ambassador Tasks';
-$stylesheets = array('/stylesheets/testimonials.css');
+$stylesheets = array('/stylesheets/testimonials.css', '/stylesheets/tasks.css');
 include('header.php');
 ?>
 
     <div class="bg">
     <h1> Hello Campus Ambassadors, <br><small>here are the list of tasks that you need to perform initally.</small></h1>
 
+    <p class="deadline">The deadline for completing all of the tasks enlisted beneath is <time>25-01-2019</time>.</p>
+    <p class="deadline">You'll be informed through our official communication channels as to what needs to be done after completion of these tasks.<br>You'll receive <time>10 reward points</time> upon successful completion.</p>
+
     <table>
     <tr>
     <td>
     <div class="container">
     <p><span>LinkedIn Profile</span></p>
-    <p>You are required to create your LinkedIn profile</p>
-    <p>Follow E-Cell GDGU on LinkedIn </p>
-    <p>Add to your profile working as a Campus Ambassador at E-Cell GDGU by JAN -19<p>
+    <p>You are required to create your own LinkedIn profile</p>
+    <p>Follow <a href="https://in.linkedin.com">E-Cell GDGU page</a> on LinkedIn </p>
+    <p>Add to your profile, working as a Campus Ambassador at E-Cell GDGU by January-2019<p>
     </div>
     </td>
     </tr>
@@ -22,16 +25,17 @@ include('header.php');
 
     <td>
     <div class="container">
-    <p><span>Social Network</span></p>
-    <p>Follow E-Cell GDGU on Instagram @ecellgdgu</p>
-    <p>Like GDGTBI Facebook page @gdgtbi</p>
-    <p>Follow E-Cell GDGU on Twitter @ecellgdgu</p>
+    <p><span>Social Networking</span></p>
+    <p>Follow E-Cell GDGU on Instagram <a href="https://instagram.com/ecellgdgu">@ecellgdgu</a>.</p>
+    <p>Like our <a href="https://facebook.com/gdgtbi">GDGTBI Facebook page</a>.</p>
+    <p>Follow E-Cell GDGU on Twitter <a href="https://twitter.com/ecellgdgu">@ecellgdgu</a>.</p>
     </div>
     </td>
     <tr>
 
     </tr>
     </table>
+
     </div>
 
 <?php
