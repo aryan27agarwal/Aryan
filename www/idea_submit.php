7adeee3d5367aa0336117df8c192fbe44719f8f6
Aryan Agarwal <aryan27agarwal@gmail.com>
2019-01-31 22:50:11 +0530

    Phone number changes

--- www/idea_submit.php
+++ www/idea_submit.php
@@ -13,7 +13,7 @@ You'll receive a confirmation at <?php echo $_POST["email1"]; ?>.<br><br>
 <?php
 
 require_once('../vendor/autoload.php');
-$submission = new Idea($_POST["name1"], $_POST["name2"], $_POST["name3"],$_POST["email1"], $_POST["email2"], $_POST["email3"], intval($_POST["phno1"]), intval($_POST["phno2"]), intval($_POST["phno3"]),
+$submission = new Idea($_POST["name1"], $_POST["name2"], $_POST["name3"],$_POST["email1"], $_POST["email2"], $_POST["email3"], intval($_POST["phone1"]), intval($_POST["phone2"]), intval($_POST["phone3"]),
 $_POST["university"], $_POST["state"], $_POST["pincode"], $_POST["city"], $_POST["title"], $_POST["idea_desc"], $_POST["stage"], $_POST["paytm"]);
 $submission->email();
 $submission->store();
