8ad905fddfad49402e1d0f4fb93fee4132d59403
Aryan Agarwal <aryan27agarwal@gmail.com>
2019-02-06 11:47:10 +0530

    Mailing setup for IDEA 19

--- lib/Idea.php
+++ lib/Idea.php
@@ -1,10 +1,10 @@
 <?php
-define('CONTACTUS_SUBJECT', "Thanks for contacting!");
-define('CONTACTUS_MESSAGE', "Thank you %s for contacting us.
-We have received your query and are looking forward to contacting you back as soon as possible.
-\nYou are receiving this message because you tried to contact us through the E-Cell GD Goenka University website.");
+define('IDEA_SUBJECT', "You've been successfuly registered!");
+define('IDEA_MESSAGE', "Thank you %s for registering for IDEA '19.
+We have received your registeration for IDEA 2019 you will be receiving a confirmation mail once your registeration is reviewed.
+\nYou are receiving this message because you have registered for IDEA 2019 through the E-Cell GD Goenka University website.");
 
-define('CONTACTUS_TG_MESSAGE', "New registeration for Idea 2019.
+define('IDEA_TG_MESSAGE', "New registeration for Idea 2019.
 *Details are:*
 ```yaml\n%s\n```");
 
@@ -15,8 +15,8 @@ class Idea {
         $this->$data = compact('name1', 'name2', 'name3', 'email1', 'email2', 'email3', 'phone1', 'phone2', 'phone3', 'university', 'state', 'pincode', 'city', 'title', 'idea_desc', 'stage', 'paytm');
     }
     public function email() {
-        $message = sprintf(CONTACTUS_MESSAGE, $this->$data['name1']);
-        return Email::sendEmail($this->$data['email1'], CONTACTUS_SUBJECT, $message);
+        $message = sprintf(IDEA_MESSAGE, $this->$data['name1']);
+        return Email::sendEmail($this->$data['email1'], IDEA_SUBJECT, $message);
     }
     public function store() {
         $sql = 'INSERT INTO idea (name1, name2, name3, email1, email2, email3, phone1, phone2, phone3, university, state, pincode, city, title, idea_desc, stage, paytm) VALUES (:name1, :name2, :name3, :email1, :email2, :email3, :phone1, :phone2, :phone3, :university, :state, :pincode, :city, :title, :idea_desc, :stage, :paytm)';
@@ -25,6 +25,6 @@ class Idea {
     }
     public function notify() {
         $part = \Symfony\Component\Yaml\Yaml::dump($this->$data);
-        return TeamUpdate::sendUpdate(sprintf(CONTACTUS_TG_MESSAGE, $part));
+        return TeamUpdate::sendUpdate(sprintf(IDEA_TG_MESSAGE, $part));
     }
 }
