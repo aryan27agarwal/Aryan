e82ae11e302bfbfd12f226f42a6d11c1714aeb3e
Aryan Agarwal <aryan27agarwal@gmail.com>
2019-02-11 16:47:14 +0530

    task updated

--- www/task.php
+++ www/task.php
@@ -5,18 +5,19 @@ include('header.php');
 ?>
 
     <div class="bg">
-    <h1> Hello Campus Ambassadors, <br><small>here are the list of tasks that you need to perform initally.</small></h1>
+    <h1> Hello Campus Ambassadors, <br><small>here is the 2nd list of tasks that you need to perform.</small></h1>
 
-    <p class="deadline">The deadline for completing all of the tasks enlisted beneath is <time>02-02-2019</time>.</p>
-    <p class="deadline">You'll be informed through our official communication channels as to what needs to be done after completion of these tasks.<br>You'll receive <time>30 reward points</time> upon successful completion.</p>
+    <p class="deadline">The deadline for completing all of the tasks enlisted beneath is <time>15-02-2019</time>.</p>
+    <p class="deadline">You'll receive <time>100 reward points</time> upon successful completion.</p>
 
     <table>
     <tr>
     <td>
     <div class="container">
-    <p><span>Social Networking</span></p>
-    <p>Follow E-Cell GDGU on Instagram <a href="https://instagram.com/ecellgdgu" target="_blank">@ecellgdgu</a>.</p>
-    <p>Like our <a href="https://facebook.com/gdgtbi" target="_blank">GDGTBI Facebook page</a>.</p>
+    <p><span>IDEA 2019</span></p>
+    <p>Share Idea 2019 poster on you Instagram story from <a href="https://www.instagram.com/ecellgdgu/" target="_blank">@ecellgdgu</a>.</p>
+    <p>Share the poster on your facebook account from <a href="https://facebook.com/gdgtbi" target="_blank">GDGTBI Facebook page</a>.</p>
+    <p>Also share the poster on your LinkedIn account from <a href="https://www.linkedin.com/company/ecellgdgu/" target="_blank">E-Cell LinkedIn profile</a>.</p>
     </div>
     </td>
     </tr>
@@ -24,17 +25,19 @@ include('header.php');
     <tr>
     <td>
     <div class="container">
-    <p><span>LinkedIn Connections</span></p>
-    <p>Follow <a href="https://www.linkedin.com/company/ecellgdgu" target="_blank">E-Cell GDGU page</a> on LinkedIn.</p>
-    <p>Add to your profile, working as a Campus Ambassador at E-Cell GDGU since January-2019.<p>
-    <p>Make sure to add a minimum 150 connections to your LinkedIn profile.</p>
-    </div>
-    </td>
+    <p><span>Get Registeration for IDEA</span></p>
+    <p>Get registrations for IDEA '19.<p>
+    <p>You will be awarded 100 points extra for each registration.</p>
+    <p>Regsitration Link: <a href="https://ecellgdgu.org/idea_register.php" target="_blank">click here</a></p>
+      <p>Last date for Idea registration 24th Feb 2019.</p>
+      <p>To download the RuleBook for the event <a href="idea\RuleBook_IDEA'19.pdf" download >click here</a></p>
+  </div>
     </tr>
     <td>
     <div class="container">
     <p><span>Submission</span></p>
-    <p>After completing these initial tasks please fill up this <a href="https://goo.gl/forms/euIVHoo89h2MfxSB2" target="_blank">google form</a> in order to complete submission procedure.</p>
+    <p>After sharing the poster on all platforms fill in this <a href="https://goo.gl/forms/S4rbcyZd7h2YgObX2" target="_blank">google form</a> in order to complete submission procedure.</p>
+    <p>For registration done on idea website preserve the paytm transaction id and fill in this <a href="https://goo.gl/forms/Ap7ihFqbDDlenJeo2" target="_blank">google form</a>.</p>
     </div>
     </td>
 
