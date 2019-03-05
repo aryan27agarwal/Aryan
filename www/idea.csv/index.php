f43b24d492b79f99bfe33a52c633e4b5017d8611
SwG Ghosh <codecrafts.cf@icloud.com>
2019-03-05 15:04:35 +0530

    Added idea.csv/index.php for listing all IDEA registration records as csv

---
+++ www/idea.csv/index.php
@@ -0,0 +1,28 @@
+<?php
+define('CSV_LIMIT', 5); // in MiB
+
+header('Content-Type: application/csv');
+$csv_file = fopen('php://temp/maxmemory:'.(CSV_LIMIT * 1024 * 1024), 'r+');
+# for header line
+fputcsv(
+    $csv_file, array(
+        'name1', 'name2', 'name3', 
+        'email1', 'email2', 'email3',
+        'phone1', 'phone2', 'phone3', 
+        'university', 'state', 'pincode',
+        'city', 'title', 'idea_desc',
+        'stage', 'paytm'
+    )
+);
+
+require('../../vendor/autoload.php');
+
+# for registration record(s) line(s)
+foreach(Idea::reglist() as $reg_record) {
+    fputcsv($csv_file, $reg_record);
+}
+
+rewind($csv_file);
+$out = stream_get_contents($csv_file);
+echo $out;
+?>
